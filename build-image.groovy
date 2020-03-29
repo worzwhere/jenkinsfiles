04857fc5633a1f325f372d10d0455f8f96e288de
WorWh3r3 <worzwhere@gmail.com>
2020-03-29 22:26:06 +0700

    first commit

---
+++ build-image.groovy
@@ -0,0 +1,26 @@
+@Library ('common') _
+pipeline {
+
+    stages {
+        stage ('Build Image') {
+            steps{
+                script{
+                    sh 'git clone '+ params.gitSource + 'repo'
+                    sh 'cd repo'
+                    sh 'chmod 775 build_image.sh'
+                    sh './build_image.sh'
+                    }
+                    echo 'Build image completed'
+        }
+        stage ('Deploy to server') {
+            when {
+                expression {
+                    return params.toDeploy == true;
+                }
+            }
+            steps{
+                build job: params.deployJobName
+            }
+        }
+    }
+}

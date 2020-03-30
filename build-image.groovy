e736ef064cf515e08822b9f2dab3cbb873c2be71
WorWh3r3 <worzwhere@gmail.com>
2020-03-30 13:21:26 +0700

    Update build-image.groovy

--- build-image.groovy
+++ build-image.groovy
@@ -1,15 +1,14 @@
 pipeline {
     agent none
     stages {
-        stage ('Build Image') {
-            steps{
-                script{
-                    sh 'git clone '+ params.gitSource + 'repo'
-                    sh 'cd repo'
-                    sh 'chmod 775 build_image.sh'
-                    sh './build_image.sh'
-                    }
-                    echo 'Build image completed'
+        stage('Build Image') {
+            steps {
+                sh 'git clone '+ params.gitSource + 'repo'
+                sh 'cd repo'
+                sh 'chmod 775 build_image.sh'
+                sh './build_image.sh'
+                echo 'Build image completed'
+            }
         }
         stage ('Deploy to server') {
             when {
@@ -19,8 +18,7 @@ pipeline {
             }
             steps{
                 build job: params.deployJobName
-                }
             }
         }
     }
-}    
+}
